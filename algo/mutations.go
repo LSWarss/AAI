13a515d7f1c13874f078afaf204a967d29f8a643
LSWarss <ls.warss98@gmail.com>
2022-04-09 16:38:21 +0200

    Added initial implementation of TSP

--- algo/mutations.go
+++ algo/mutations.go
@@ -3,8 +3,8 @@ package algo
 // Inversion Mutation algorithm.
 // Takes in array of individuals from population,
 // calculates splits in the individual array by len of it and mutationRange as end range.
-func InversionMutation(individual []int, mutationRate float32) (newIndividual []int) {
-	randRange := int(float32(len(individual)) * mutationRate)
+func inversionMutation(individual []int, mutationRate float64) (newIndividual []int) {
+	randRange := int(float64(len(individual)) * mutationRate)
 	x := randomInt(0, randRange)
 	y := randomInt(x, randRange)
 
@@ -13,4 +13,12 @@ func InversionMutation(individual []int, mutationRate float32) (newIndividual []
 	newIndividual = append(newIndividual, individual[:x]...)
 
 	return newIndividual
+}
+
+func MakeInversionMutation(population [][]int, mutationRate float64) (mutatedPopulation [][]int) {
+	for _, individual := range population {
+		mutatedPopulation = append(mutatedPopulation, inversionMutation(individual, mutationRate))
+	}
+
+	return mutatedPopulation
 }
---
+++ algo/tsp.go
@@ -0,0 +1,48 @@
+package algo
+
+import (
+	"fmt"
+
+	distances "github.com/lswarss/AAI/files"
+)
+
+func createPopulationWithFitness(population [][]int, fitness []int) (scoredPopulation PopulationWithFitness) {
+	for i := 0; i < len(fitness); i++ {
+		scoredPopulation.ScoredPopulation = append(scoredPopulation.ScoredPopulation, [][]int{population[i], {fitness[i]}})
+	}
+	scoredPopulation.PopulationSize = len(scoredPopulation.ScoredPopulation)
+
+	return scoredPopulation
+}
+
+func getPopulationFromSelection(selection [][][]int) (population [][]int) {
+	for i := 0; i < len(selection); i++ {
+		population = append(population, selection[i][0])
+	}
+
+	return population
+}
+
+func nextGeneration(distancesMatrix distances.DistanceMatrix, population [][]int, mutationRate float64) (nextGeneration [][]int) {
+	populationFitness := CountFitness(distancesMatrix, population)
+	scoredPopulation := createPopulationWithFitness(population, populationFitness)
+	selection := MakeTournament(scoredPopulation)
+	populationFromSelection := getPopulationFromSelection(selection)
+	breed := MakeCrossover(populationFromSelection)
+	nextGeneration = MakeInversionMutation(breed, mutationRate)
+
+	return nextGeneration
+}
+
+func GeneticAlgorithm(distancesMatrix distances.DistanceMatrix, populationSize int, mutationRate float64, generations int) (bestIndividual []int, bestScore int) {
+	initialPop := CreatePopulationMatrix(distancesMatrix, populationSize)
+
+	for i := 0; i <= generations; i++ {
+		fmt.Println("Generation number: ", i)
+		initialPop.Population = nextGeneration(distancesMatrix, initialPop.Population, mutationRate)
+		bestIndividual, bestScore := GetBestFitnessAndIndividual(distancesMatrix, initialPop.Population)
+		fmt.Println("Scores: ", bestIndividual, bestScore)
+	}
+
+	return GetBestFitnessAndIndividual(distancesMatrix, initialPop.Population)
+}
